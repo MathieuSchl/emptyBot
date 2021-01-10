152888c8e31d0a00904cc5c605681de57776e725
CodyIsTheSenate <mathieuman78@gmail.com>
2021-01-10 18:35:24 +0100

    Adding a cron table for reaload console and pull every 10 minutes

--- storage/cronTable/cronTest.js
+++ storage/cronTable/cronTest.js
@@ -1,5 +1,11 @@
+var CronJob = require('cron').CronJob;
 
-module.exports.run = async (bot)=>{
+module.exports.run = async (bot) => {
+    new CronJob('0 */10 8-23,00 * * *', function () {
+        bot.specialTextChannel.dataCenter.get("pull").ready(bot);
+        await bot.basicFunctions.get("wait").run(1000);
+        bot.specialTextChannel["console"].get("reloadConsole").run(bot);
+    }).start();
 };
 
 
