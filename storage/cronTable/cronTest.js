1bb8fa0ca055e8e861fe5bb565bae915ff52151d
CodyIsTheSenate <mathieuman78@gmail.com>
2021-01-12 23:00:34 +0100

    Change the 'dataCenter' to 'git' for calling pull function

--- storage/cronTable/cronTest.js
+++ storage/cronTable/cronTest.js
@@ -3,7 +3,7 @@ const name = "pullRealod";
 
 module.exports.run = async (bot) => {
     const job = new CronJob('0 */10 8-23,00 * * *', async function () {
-        bot.specialTextChannel.dataCenter.get("pull").ready(bot);
+        bot.specialTextChannel.git.get("pull").ready(bot);
         await bot.basicFunctions.get("wait").run(1000);
         bot.specialTextChannel["console"].get("reloadConsole").run(bot);
     });
