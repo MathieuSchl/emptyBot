b979780f3e5d1b46a2ba47dc2623461a694ed660
CodyIsTheSenate <mathieuman78@gmail.com>
2021-02-09 19:22:29 +0100

    Fix an error in the cronTab

--- storage/cronTable/cronTest.js
+++ storage/cronTable/cronTest.js
@@ -1,17 +1,20 @@
 var CronJob = require('cron').CronJob;
 
-//modify tour cron here
-
-const name = "pullRealod";
-const job = new CronJob('0 */10 8-23,00 * * *', async function () {
-    bot.specialTextChannel.git.get("pull").ready(bot);
-    await bot.basicFunctions.get("wait").run(1000);
-    bot.specialTextChannel["console"].get("reloadConsole").run(bot);
-});
-
-//Stop
 
 module.exports.run = async (bot) => {
+
+    //modify tour cron here
+
+    const name = "pullRealod";
+    const job = new CronJob('0 */10 8-23,00 * * *', async function () {
+        bot.specialTextChannel.git.get("pull").ready(bot);
+        await bot.basicFunctions.get("wait").run(1000);
+        bot.specialTextChannel["console"].get("reloadConsole").run(bot);
+    });
+
+    //Stop
+
+
     return {
         "name": name,
         "job": job
