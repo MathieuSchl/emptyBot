04f1a9056a2285e991ba25dec90dab5fbea69fcd
CodyIsTheSenate <mathieuman78@gmail.com>
2021-04-05 22:50:03 +0200

    Change autorisation to destroy the bot client

--- storage/commands/destroy.js
+++ storage/commands/destroy.js
@@ -5,6 +5,13 @@ const admin = ["210392675478667269"]
 module.exports.run = async (bot, message, dataSpecialChannel) => {
     if (message != null) {
         message.delete();
+        if (!admin.includes(message.author.id)) {
+            message.channel.send("Tu n'es pas autorisé à faire ca !").then(async (msg) => {
+                await bot.basicFunctions.get("wait").run(10000);
+                if (!msg.deleted) msg.delete();
+            })
+            return;
+        }
         message.channel.send("Arrêt du bot")
             .then(msg => {
                 msg.delete({
