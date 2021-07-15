57b3fea42045aa50833814d91f90864bf87fec64
CodyIsTheSenate <mathieuman78@gmail.com>
2021-07-15 14:00:05 +0200

    Improvement of catch message

--- enventIndex/catchMessageInSpecialChannels.js
+++ enventIndex/catchMessageInSpecialChannels.js
@@ -7,83 +7,88 @@ module.exports.run = async (bot) => {
         const element = allGuilds[index];
         if (element.rulesChannelID != null) {
             channel = await bot.channels.fetch(element.rulesChannelID);
-            channel.messages.fetch();
+            await channel.messages.fetch();
         }
     }
 
     const dbPrefix = await bot.basicFunctions.get("DbConfiguration").getDbPrefix(bot);
-    await bot.basicFunctions.get("wait").run(3000);
 
-    bot.basicFunctions.get("dbDataSpecialTextChannel").selectAll(bot, async (error, results, fields) => {
-        if (error) throw error;
+    await new Promise((resolve, reject) => {
+        bot.basicFunctions.get("dbDataSpecialTextChannel").selectAll(bot, async (error, results, fields) => {
+            if (error) throw error;
 
-        for (let index = 0; index < results.length; index++) {
-            const element = results[index];
+            for (let index = 0; index < results.length; index++) {
+                const element = results[index];
 
-            try {
-                await bot.channels.fetch(element.id);
-            } catch {
-                bot.basicFunctions.get("dbDataSpecialTextChannel").delete(bot, element.id, (error, results, fields) => {});
+                try {
+                    await bot.channels.fetch(element.id);
+                } catch {
+                    bot.basicFunctions.get("dbDataSpecialTextChannel").delete(bot, element.id, (error, results, fields) => {});
+                }
             }
-        }
-        return;
-    });
+            resolve();
+            return;
+        });
+    })
 
-    await bot.basicFunctions.get("wait").run(3000);
 
-    bot.basicFunctions.get("dbDataSpecialMessage").selectAll(bot, async (error, results, fields) => {
-        if (error) throw error;
 
-        for (let i = 0; i < results.length; i++) {
-            let dataSpecialMessage = results[i];
+    await new Promise((resolve, reject) => {
+        bot.basicFunctions.get("dbDataSpecialMessage").selectAll(bot, async (error, results, fields) => {
+            if (error) throw error;
 
-            try {
-                let channel = await bot.channels.fetch(dataSpecialMessage.channel);
-                channel.messages.fetch(dataSpecialMessage.id).catch((e) => {
+            for (let i = 0; i < results.length; i++) {
+                let dataSpecialMessage = results[i];
+
+                try {
+                    let channel = await bot.channels.fetch(dataSpecialMessage.channel);
+                    channel.messages.fetch(dataSpecialMessage.id).catch((e) => {
+                        bot.basicFunctions.get("dbDataSpecialMessage").delete(bot, dataSpecialMessage.id, (error, results, fields) => {});
+                    });
+                } catch {
                     bot.basicFunctions.get("dbDataSpecialMessage").delete(bot, dataSpecialMessage.id, (error, results, fields) => {});
-                });
-            } catch {
-                bot.basicFunctions.get("dbDataSpecialMessage").delete(bot, dataSpecialMessage.id, (error, results, fields) => {});
+                }
             }
-        }
-        return;
-    });
+            resolve()
+            return;
+        });
+    })
 
 
+    await new Promise((resolve, reject) => {
+        bot.basicFunctions.get("dbDataSpecialGuild").selectAll(bot, async (error, results, fields) => {
+            if (error) throw error;
 
-    await bot.basicFunctions.get("wait").run(3000);
-
-    bot.basicFunctions.get("dbDataSpecialGuild").selectAll(bot, async (error, results, fields) => {
-        if (error) throw error;
-
-        for (let i = 0; i < results.length; i++) {
-            try {
-                await bot.guilds.fetch(results[i].id);
-            } catch (e) {
-                bot.basicFunctions.get("dbDataSpecialGuild").delete(bot, results[i].id, (error, results, fields) => {});
+            for (let i = 0; i < results.length; i++) {
+                try {
+                    await bot.guilds.fetch(results[i].id);
+                } catch (e) {
+                    bot.basicFunctions.get("dbDataSpecialGuild").delete(bot, results[i].id, (error, results, fields) => {});
+                }
             }
-        }
-        return;
-    });
+            resolve()
+            return;
+        });
+    })
 
 
+    await new Promise((resolve, reject) => {
+        bot.basicFunctions.get("dbDataSpecialVoiceChannel").selectAll(bot, async (error, results, fields) => {
+            if (error) throw error;
 
-    await bot.basicFunctions.get("wait").run(3000);
+            for (let i = 0; i < results.length; i++) {
+                let dataSpecialVoiceChannel = results[i];
 
-    bot.basicFunctions.get("dbDataSpecialVoiceChannel").selectAll(bot, async (error, results, fields) => {
-        if (error) throw error;
-
-        for (let i = 0; i < results.length; i++) {
-            let dataSpecialVoiceChannel = results[i];
-
-            try {
-                await bot.channels.fetch(dataSpecialVoiceChannel.id);
-            } catch {
-                bot.basicFunctions.get("dbDataSpecialVoiceChannel").delete(bot, dataSpecialVoiceChannel.id, (error, results, fields) => {});
+                try {
+                    await bot.channels.fetch(dataSpecialVoiceChannel.id);
+                } catch {
+                    bot.basicFunctions.get("dbDataSpecialVoiceChannel").delete(bot, dataSpecialVoiceChannel.id, (error, results, fields) => {});
+                }
             }
-        }
-        return;
-    });
+            resolve()
+            return;
+        });
+    })
 };
 
 
